b1bde18610ace191a1a6528a0dafbbce6b9354a9
womenoasis <leidybeatrizvegaarguello@gmail.com>
2024-05-08 05:08:28 -0500

    feat: :sparkles: Mi primer ejercicio
    python
    
    by: lady vega

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/womenoasis.py
@@ -0,0 +1,27 @@
+#https://www.python.org/
+
+#comentario en una linea
+'''
+Esto es un comentario
+en varias lineas 
+'''
+
+"""
+Esto es un comentario 
+en varias lineas
+"""
+
+#crear una variable
+women = "Mi variable"
+#crear una constante
+My_constant = "mi constante" #por conveccion 
+
+#Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
+#Datos primitivos
+my_int = 1
+my_float = 1.5
+my_bool = True
+my_bool = False
+my_str = "Mi cadena de texto"
+
+print("Hola, python")
