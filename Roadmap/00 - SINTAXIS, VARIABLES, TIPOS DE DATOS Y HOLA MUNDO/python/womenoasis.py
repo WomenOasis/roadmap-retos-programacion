f0ef0eea6cdc1560a2b40eff37a58c056e445415
WomenOasis <leidybeatrizvegaarguello@gmail.com>
2024-05-11 12:50:03 -0500

    feat: :sparkles: Segundo ejercicio python
    by: lady vega

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/womenoasis.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/womenoasis.py
@@ -14,14 +14,19 @@ en varias lineas
 #crear una variable
 women = "Mi variable"
 #crear una constante
-My_constant = "mi constante" #por conveccion 
+My_CONSTANT = "mi constante" #por conveccion 
 
 #Crea variables representando todos los tipos de datos primitivos del lenguaje (cadenas de texto, enteros, booleanos...).
 #Datos primitivos
-my_int = 1
+my_int: int = 1
 my_float = 1.5
 my_bool = True
 my_bool = False
 my_str = "Mi cadena de texto"
 
 print("Hola, python")
+
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_str))
---
+++ Roadmap/01 - OPERADORES Y ESTRUCTURAS DE CONTROL/python/womenoasis.py
@@ -0,0 +1,44 @@
+# 1 Crea ejemplos utilizando todos los tipos de operadores de tu lenguaje: Aritméticos, lógicos, de comparación, asignación, identidad, pertenencia, bits... (Ten en cuenta que cada lenguaje puede poseer unos diferentes)
+
+
+#tipo de operadores (Aritmeticos suma, resta, multiplicacion, dividir, resta)
+
+#Aritmeticos
+
+suma = 1 + 1
+resta = 1 - 1
+multiplicacion = 1 * 1
+dividir = 1 / 1
+
+print(suma)
+print(resta)
+print(multiplicacion)
+print(dividir)
+
+#Logicos (AND, OR, NOT)
+
+
+#Comparacion =IGUALDAD (==) DESIGUALDAD (!==) Mayor que (>) Menor que (<) Mayor o igual que (>=) Menor o igual que (<=)
+
+
+#AAsignación (=) Asignación con suma (+=) Asignación con resta (-=) Asignación con multiplicación (*=) Asignación con división (/=) Asignación con módulo (%=)
+
+
+#Identidad is - is not
+
+
+#pertenencia in- not in
+
+
+#bits AND a nivel de bits (&)  OR a nivel de bits (|)  XOR a nivel de bits (^) Complemento a nivel de bits (~)  Desplazamiento a la izquierda (<<) Desplazamiento a la derecha (>>)
+
+
+# 2. Utilizando las operaciones con operadores que tú quieras, crea ejemplos que representen todos los tipos de estructuras de control que existan en tu lenguaje: Condicionales, iterativas, excepciones...+
+
+
+# 3. Debes hacer print por consola del resultado de todos los ejemplos.
+
+
+
+# 4. 
+# }DIFICULTAD EXTRA (opcional): Crea un programa que imprima por consola todos los números comprendidos entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
